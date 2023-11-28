8485f61776f3f1ca3fcb58dfd6f9b1f3d847d77c
Al Ghani <alghani63@gmail.com>
2023-11-28 18:42:26 +0700

    3 - View Data Barang - Muhamad Naufal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import AddBarang from "./components/formDataBarang" 
+import BarangList from "./components/listBarang"
+
 import './App.css'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<AddBarang/>}/>
+      <Route path="/" element={<BarangList/>}/>
+        <Route path="/addBarang" element={<AddBarang/>}/>
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/components/listBarang.jsx
@@ -0,0 +1,68 @@
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+import Navbar from './navbar';
+import { Link } from "react-router-dom";
+
+const BarangList = () => {
+const [barangs, setBarang] = useState([]);
+
+useEffect(()=>{
+    getBarangs(); 
+}, []);
+
+const getBarangs = async () =>{
+    const response = await axios.get('http://localhost:3000/barang');
+    setBarang(response.data);
+}
+
+const deleteUser = async (id) => {
+    try{
+        await axios.delete(`http://localhost:3000/barang/${id}`);
+        getBarangs();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+  return (
+    <div className="container">
+        <Navbar />
+        <div className="columns mt-5 is-centered">
+            <div className="column is-half">
+                <Link to={`addBarang`} className="button is-success">
+                    Add New
+                </Link>
+                <tabel className = "table is-striped is-fullwidth">
+                    <thead>
+                        <tr>
+                            <th>kodebarang</th>
+                            <th>namabarang</th>
+                            <th>satuan</th>
+                            <th>hargasatuan</th>
+                            <th>stok</th>
+                            {/* <th>Action</th> */}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {barangs.map((barang, index) => (
+                        <tr key={barang.id}>
+                            <td>{barang.kodebarang}</td>
+                            <td>{barang.namabarang}</td>
+                            <td>{barang.satuan}</td>
+                            <td>{barang.hargasatuan}</td>
+                            <td>{barang.stok}</td>
+                            {/* <td>
+                                <Link to={`edit/${user.id}`} className="button is-small is-info">Edit</Link>
+                                <button onClick={()=> deleteUser(user.id)} className="button is-small is-danger">Delete</button>
+                            </td> */}
+                        </tr>
+                        ))}
+                    </tbody>
+                </tabel>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default BarangList;
